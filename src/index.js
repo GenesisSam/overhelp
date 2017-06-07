5349b37ebf1022d0c5cfd337430b2a2a1dc42859
GenesisSam <donghyun.lee@vingle.net>
2017-06-07 10:59:15 +0900

    Add axios & test stackoverflow api

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var fs = require("fs")
-
-app.set('views', __dirname + '/views');
+var axios = require('axios');
 
 var server = app.listen(3000, function(){
  console.log("Express server has started on port 3000")
 });
-
-app.use(express.static('public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
@@ -19,6 +15,25 @@ app.use(session({
  resave: false,
  saveUninitialized: true
 }));
+
+app.get("/overhelp", (req, res) => {
+  try{
+    axios.get("https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=out%20of%20range&site=stackoverflow&filter=!b0OfNFICOao(Ho")
+    .then((response) => {
+      console.log(response.data);
+      res.json({
+        data: response.data
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+  } catch(err) {
+    console.log(err);
+    res.json(err);
+  }
+});
 
 
 // var router = require('./router/main')(app, fs);
