a615680c5e716d47fb9ede9e239f71889df3de2c
GenesisSam <samuel940401@gmail.com>
2017-06-13 22:09:16 +0900

    Change port number && remove comments

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
-var server = app.listen(3000, function(){
- console.log("Express server has started on port 3000")
+var server = app.listen(3030, function(){
+ console.log("Express server has started on port 3030")
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(session({
- secret: '@#@$MYSIGN#@$#$',
- resave: false,
- saveUninitialized: true
-}));
 
 var router = require("./routes")(app);
-
-// var router = require('./router/main')(app, fs);
