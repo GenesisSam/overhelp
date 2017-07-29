f94a218b6a2a1adc0d68a92cf85ead714a1140ad
Sung-jin <osj4872@gmail.com>
2017-07-29 18:23:29 +0900

    Add c language error paser

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
-var bodyParser = require('body-parser');
-
-var server = app.listen(3030, function(){
- console.log("Express server has started on port 3030")
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 var router = require("./routes")(app);
+
+var server = app.listen(3030, () => {
+  console.log("Express server has started on port 3030");
+});
--- src/routes/overhelp.js
+++ src/routes/overhelp.js
@@ -11,28 +11,56 @@ module.exports = (express) => {
   express.post("/overhelp", (req, res) => {
     const reqTargetPlatform = req.body.platform;
     const reqError = req.body.error;
+    let sendingError;
 
-    if (!reqTargetPlatform || !reqError) {
+    if(!reqTargetPlatform || !reqError) {
       res.status(400);
       res.json({ error: "Bad parameters" });
       return;
     }
 
-    try{
-      axios.get(`https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=${reqError}&site=stackoverflow&filter=!)(pDGc.tZ8HL2BJ6xlo-Xs4P)X9IJjTki3ZdKn30pDjT(fSPjV3tf`)
-      .then((response) => {
-        console.log("axios done");
-        res.json({
-          data: response.data
-        })
-      })
-      .catch((err) => {
-        console.log("axios error");
-        res.json(err);
-      });
-    } catch(err) {
-      console.log("catch fires!!");
-      res.json(err);
+    const parsedError = errorStackParse(reqError);
+
+    if (!parsedError || parsedError.length === 0) {
+      res.status(400);
+      res.json({ error: "Error message parse failed" });
+      return;
     }
+
+    sendingError = parsedError[0].trim();
+
+    axios.get(`https://api.stackexchange.com/2.2/search?tagged=${reqTargetPlatform}&order=desc&sort=activity&intitle=${sendingError}&site=stackoverflow&filter=!b05-dkWi.xMT(m`)
+    .then((response) => {
+      console.log("axios done");
+      if (response.data.items.length === 0) {
+        // TODO: should call real user matching funciton.
+        res.json({ call: "open QnA channel" });
+        return;
+      }
+      res.json({
+        data: response.data
+      });
+    })
+    .catch((err) => {
+      console.log("axios error", err);
+      res.json(err);
+    });
+  });
+}
+
+
+function errorStackParse(errors) {
+  const C_REGEX_PATTERN_01 = /error \w+/;
+  const C_REGEX_PATTERN_RUNTIME = /^Run-Time Check Failure/;
+
+  // FIRST CHECK RUN-TIME error
+  if(errors.match(C_REGEX_PATTERN_RUNTIME)) {
+    return errors;
+  }
+
+  const error = errors.replace(":\\", "");
+  const splitedError = error.split(":");
+  return splitedError.filter((message) => {
+    return message.trim().match(C_REGEX_PATTERN_01);
   });
 }
