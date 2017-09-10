28745f7ac1b421c25fd9553a2090db609c07b383
Sung-jin <osj4872@gmail.com>
2017-09-10 16:15:17 +0900

    Branch (#5)
    * Add c language error paser
    
    * Add mysql connection && StackOverFlow API result save
    
    * Apply feedback

--- src/routes/overhelp.js
+++ src/routes/overhelp.js
@@ -1,4 +1,5 @@
 var axios = require('axios');
+var mysql = require('mysql');
 
 module.exports = (express) => {
   /**
@@ -12,6 +13,7 @@ module.exports = (express) => {
     const reqTargetPlatform = req.body.platform;
     const reqError = req.body.error;
     let sendingError;
+
 
     if(!reqTargetPlatform || !reqError) {
       res.status(400);
@@ -31,15 +33,72 @@ module.exports = (express) => {
 
     axios.get(`https://api.stackexchange.com/2.2/search?tagged=${reqTargetPlatform}&order=desc&sort=activity&intitle=${sendingError}&site=stackoverflow&filter=!b05-dkWi.xMT(m`)
     .then((response) => {
-      console.log("axios done");
+
+      if (response.data.items.length !== 0){
+        let accpetedAnswerBody;
+        let resultTitle = response.data.items[0].title;
+        let resultTags = response.data.items[0].tags;
+        let resultLink = response.data.items[0].link;
+
+
+        const acceptedAnswer = response.data.items[0].answers.find((answer) => {
+          return answer.is_accepted;
+        });
+
+        if (acceptedAnswer) {
+          accpetedAnswerBody = acceptedAnswer.body;
+        } else {
+          // If all questions are not answered, here is the solution
+        }
+
+        //DB login when data exists
+
+        var connection = mysql.createConnection({
+          host : '127.0.0.1',
+          user : 'root',
+          password : '1q2w3e4r',
+          database: 'overhelpDB'
+        });
+
+        connection.connect((err) => {
+          if(err) {
+            throw err;
+          } else {
+            console.log("DB Connection done");
+          }
+        });
+
+        let stringifiedTags = resultTags.join(",");
+
+        const data = { title : resultTitle, tag : stringifiedTags, link : resultLink, body : accpetedAnswerBody};
+
+        connection.query('INSERT INTO answerInfo SET ?', data, (err) => {
+          if (err){
+            console.log(err);
+          }
+        });
+
+        //unconnect
+        connection.end();
+
+        res.status(200);
+        res.json(data);
+
+        // TODO: 키워드화
+        // const resultTitle = response.data.items.title;
+        // axios.get('http://api.datamixi.com/datamixiApi/keywordextract?request=post&request_id=0&text=${resultTitle}')
+        // .then((responseT) => {
+        //   //Receive and process data
+        //   //need key value
+        //  })
+      }
+
       if (response.data.items.length === 0) {
         // TODO: should call real user matching funciton.
         res.json({ call: "open QnA channel" });
         return;
       }
-      res.json({
-        data: response.data
-      });
+
     })
     .catch((err) => {
       console.log("axios error", err);
@@ -50,14 +109,13 @@ module.exports = (express) => {
 
 
 function errorStackParse(errors) {
-  const C_REGEX_PATTERN_01 = /error \w+/;
+  const C_REGEX_PATTERN_01 = /(Error|error) \w+/;
   const C_REGEX_PATTERN_RUNTIME = /^Run-Time Check Failure/;
 
   // FIRST CHECK RUN-TIME error
   if(errors.match(C_REGEX_PATTERN_RUNTIME)) {
-    return errors;
+    return [errors];
   }
-
   const error = errors.replace(":\\", "");
   const splitedError = error.split(":");
   return splitedError.filter((message) => {
